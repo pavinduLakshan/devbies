d3f469fe44d745c02ea693eea6be588bf3c521aa
pavinduLakshan <174084F@uom.lk>
2021-09-02 12:44:09 +0530

    implement home page with search by tags

--- src/components/Freebie.js
+++ src/components/Freebie.js
@@ -1,12 +1,52 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    cardGrid: {
+      paddingTop: theme.spacing(8),
+      paddingBottom: theme.spacing(8),
+    },
+    card: {
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    cardMedia: {
+      paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+      flexGrow: 1,
+    }
+  }));
 
 const Freebie = ({imgPath,name,desc,url}) => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <img src={imgPath} height="150" width="150"/>
-            <h3><a href={url} target="_blank">{name}</a></h3>
-            <p>{desc}</p>
-        </div>
+        <Grid item xs={12} sm={6} md={4} spacing={3}>
+            <Card className={classes.card}>
+                <CardMedia
+                    className={classes.cardMedia}
+                    image={imgPath}
+                    title={name}
+                />
+                <CardContent className={classes.cardContent}>
+                    <a href={url} target="_blank">
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {name}
+                    </Typography>
+                    </a>
+                    <Typography style={{textAlign: "justify"}}>
+                        {desc}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Grid>
     );
 };
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,34 +1,68 @@
 import React, { useState, useEffect } from 'react';
+import {NavLink} from "react-router-dom"
+import { makeStyles } from '@material-ui/core/styles';
+import {Grid,Typography, Container} from '@material-ui/core';
 import Freebie from "../components/Freebie"
-import freebies from "../data/freebies.json"
+import {freebies} from "../data"
+
+const useStyles = makeStyles((theme) => ({
+    tagContainer: {
+        display: "flex",
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "center"
+    },
+    tagItem: {
+        padding: "1%",
+        margin: "1%",
+        textDecoration: "none",
+        cursor: "pointer",
+        backgroundColor: "black",
+        color: "white"
+    },
+    cardGrid: {
+      paddingTop: theme.spacing(8),
+      paddingBottom: theme.spacing(8)
+    }
+}));
 
 const Home = () => {
+    const classes = useStyles();
+    const params = new URLSearchParams(window.location.search)
+    const selectedTagParam = params.get("tag")
     const [tags,setTags] = useState([])
-    const [selectedTag,setSelectedTag] = useState()
 
     useEffect(() => {
         setTags(Object.keys(freebies))
     },[])
 
     return (
-        <div>
-            <h1>Freebies for Developers</h1>
- 
-            {tags.length > 0 && tags.map(tag => <div onClick={() => setSelectedTag(tag)}>
-                {tag}
-            </div>)}
+        <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h3" component="h2">Freebies for Developers</Typography>
+        </Grid>
+        <Grid item xs={12} className={classes.tagContainer}>
+          {tags.length > 0 && tags.map(tag => <NavLink className={classes.tagItem} to={"/?tag="+tag}>
+                {tag}  
+              </NavLink>)}
+        </Grid>
 
-           {freebies[selectedTag]?.length > 0 && freebies[selectedTag].map((freebie,index) => {
-               console.log(freebie)
-               return (<Freebie 
+        <Container className={classes.cardGrid}>
+          <Grid container spacing={4}>
+          {freebies[selectedTagParam]?.length > 0 && freebies[selectedTagParam].map((freebie,index) => {
+            return (
+                <Freebie
                 key={index}
                 imgPath={freebie.img}
                 name={freebie.name}
                 desc={freebie.desc}
                 url={freebie.url}
-              />)
+            />
+            )
            })}
-        </div>
+          </Grid>
+        </Container>
+      </Grid>
     );
 };
 
