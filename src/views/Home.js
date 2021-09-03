1dad64594bf2f16f8348a65c848b7e1c34ae2d52
pavinduLakshan <174084F@uom.lk>
2021-09-03 13:35:52 +0530

    improve styles of the home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import {NavLink} from "react-router-dom"
 import { makeStyles } from '@material-ui/core/styles';
-import {Grid,Typography, Container} from '@material-ui/core';
+import {Grid,Typography, Container, Chip, Box} from '@material-ui/core';
 import Freebie from "../components/Freebie"
 import {freebies} from "../data"
 
 const useStyles = makeStyles((theme) => ({
+    titleContainer: { 
+      position: "sticky", 
+      top: 0, 
+      right: 0,
+      zIndex: 10,
+      backgroundColor: "#F26BEE",
+      width: "100%" 
+    },
     tagContainer: {
         display: "flex",
         width: "100%",
@@ -17,12 +25,16 @@ const useStyles = makeStyles((theme) => ({
         margin: "1%",
         textDecoration: "none",
         cursor: "pointer",
-        backgroundColor: "black",
-        color: "white"
+    },
+    tag: {
+        cursor: "pointer",
+        "&:hover": {
+          transform: "scale(1.4)"
+        }
     },
     cardGrid: {
-      paddingTop: theme.spacing(8),
-      paddingBottom: theme.spacing(8)
+      paddingTop: theme.spacing(4),
+      paddingBottom: theme.spacing(8),
     }
 }));
 
@@ -38,16 +50,26 @@ const Home = () => {
 
     return (
         <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Typography variant="h3" component="h2">Freebies for Developers</Typography>
+        <div className={classes.titleContainer}>
+          <Grid item xs={12} >
+          <Typography style={{marginTop: "1%",fontFamily: "ZCOOL KuaiLe, cursive"}} variant="h4" component="h4">Freebies for Developers</Typography>
+          <Typography style={{marginLeft: "auto", marginRight: "auto",marginTop: "1%",width: "50%"}} variant="h6" component="h4">
+          A curated collection of 1000+ free developer resources
+          </Typography>
         </Grid>
         <Grid item xs={12} className={classes.tagContainer}>
-          {tags.length > 0 && tags.map(tag => <NavLink className={classes.tagItem} to={"/?tag="+tag}>
-                {tag}  
-              </NavLink>)}
+          {tags.length > 0 && tags.map(tag => <NavLink className={classes.tagItem} to={"/?tag=" + tag}>
+            <Chip
+              className={classes.tag}
+              size="medium"
+              color={selectedTagParam === tag ? "secondary" : "primary" }
+              label={tag}
+            />
+          </NavLink>)}
         </Grid>
+          </div>
 
-        <Container className={classes.cardGrid}>
+<Container className={classes.cardGrid}>
           <Grid container spacing={4}>
           {freebies[selectedTagParam]?.length > 0 && freebies[selectedTagParam].map((freebie,index) => {
             return (
@@ -62,7 +84,8 @@ const Home = () => {
            })}
           </Grid>
         </Container>
-      </Grid>
+     
+     </Grid>
     );
 };
 
