b5e33cc8fa1a8ff2c1e14ff22ca14927ab02ccbe
pavinduLakshan <174084F@uom.lk>
2021-09-03 17:18:11 +0530

    add more freebies

--- src/data/freebies.js
+++ src/data/freebies.js
@@ -2,10 +2,12 @@ import css from "./css.json"
 import free_trial from "./free_trial.json"
 import illustrations from "./illustrations.json"
 import icons from "./icons.json"
+import fonts from "./fonts.json"
 
 export const freebies = {
     "css": css,
     "free trials": free_trial,
     "illustrations": illustrations,
-    "icons": icons
+    "icons": icons,
+    "fonts": fonts
 }
