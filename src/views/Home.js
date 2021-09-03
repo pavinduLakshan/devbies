41ada43718f22b7c9099d3d340b24f3c08fe6714
pavinduLakshan <174084F@uom.lk>
2021-09-03 13:37:53 +0530

    remove unused imports

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {NavLink} from "react-router-dom"
 import { makeStyles } from '@material-ui/core/styles';
-import {Grid,Typography, Container, Chip, Box} from '@material-ui/core';
+import {Grid,Typography, Container, Chip} from '@material-ui/core';
 import Freebie from "../components/Freebie"
 import {freebies} from "../data"
 
