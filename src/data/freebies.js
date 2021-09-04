a7d2462c4c47c95ced42e13dbfba2c1b961649fd
pavinduLakshan <174084F@uom.lk>
2021-09-05 01:47:03 +0530

    add remote job listing websites

--- src/data/freebies.js
+++ src/data/freebies.js
@@ -3,11 +3,13 @@ import free_trial from "./free_trial.json"
 import illustrations from "./illustrations.json"
 import icons from "./icons.json"
 import fonts from "./fonts.json"
+import remote_jobs from "./remote_jobs.json"
 
 export const freebies = {
     "css": css,
     "free trials": free_trial,
     "illustrations": illustrations,
     "icons": icons,
-    "fonts": fonts
+    "fonts": fonts,
+    "remote jobs": remote_jobs
 }
