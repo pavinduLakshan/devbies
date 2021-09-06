808af4273a78216975980f467227cd5b0f71fa0d
pavinduLakshan <174084F@uom.lk>
2021-09-06 15:17:35 +0530

    add new freebies of different categories

--- src/data/freebies.js
+++ src/data/freebies.js
@@ -5,13 +5,21 @@ import icons from "./icons.json"
 import fonts from "./fonts.json"
 import remote_jobs from "./remote_jobs.json"
 import images from "./images.json"
+import roadmaps from "./roadmaps.json"
+import cheat_sheets from "./cheat_sheets.json"
+import performance from "./performance.json"
+import templates from "./templates.json"
 
 export const freebies = {
     "css": css,
+    "cheat sheets": cheat_sheets,
     "fonts": fonts,
     "free trials": free_trial,
     "icons": icons,
     "illustrations": illustrations,
     "images": images,
-    "remote jobs": remote_jobs
+    "performance": performance,
+    "remote jobs": remote_jobs,
+    "roadmaps": roadmaps,
+    "templates": templates
 }
